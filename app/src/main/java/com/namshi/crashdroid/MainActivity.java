ea7d1b8222fd672c7149859f0380babfeb8e0a8b
vgaidarji <veaceslav.gaidarji@yopeso.com>
2015-10-15 11:40:00 +0300

    MA-2106 inject services layout. It will contain checkboxes with services. We can enable/disable service.

--- app/src/main/java/com/namshi/crashdroid/MainActivity.java
+++ app/src/main/java/com/namshi/crashdroid/MainActivity.java
@@ -1,13 +1,21 @@
 package com.namshi.crashdroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.servicesLayout)
+    LinearLayout servicesLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
